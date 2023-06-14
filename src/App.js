dde8cfc133b4b6c573bc18c128d05c6713a86414
Mohit <mohitfiverr007@gmail.com>
2023-06-14 09:31:04 +0530

    final-code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resturant from "./component/Basic/Resturant";
+
+const App = () => {
+  return (
+    <>
+      <Resturant />
+    </>
+  );
+};
+
+export default App;
